83502bb163c42b13c4417784dc04399f52bc3822
mtcnbzks <mtcnbzks@gmail.com>
2022-11-15 13:08:40 +0300

    blacklist feature

--- index.js
+++ index.js
@@ -26,17 +26,21 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", (msg) => {
-  if (msg.author.id == "313284967214088196") {
-    msg.react("💯");
-  }
-});
+// client.on("message", (msg) => {
+//   if (msg.author.id == "313284967214088196") {
+//     msg.react("💯");
+//   }
+// });
+
+// client.on("message", (msg) => {
+//   if (greetingList.includes(msg.content.toLowerCase())) {
+//     msg.reply("Aleyküm selam.");
+//   }
+// });
 
 client.on("message", (msg) => {
-  if (greetingList.includes(msg.content.toLowerCase())) {
-    msg.reply("Aleyküm selam.");
-  }
-  if (blacklist.includesmsg.content.toLowerCase()) {
+  // if msg contains any blacklist word
+  if (blacklist.some((word) => msg.content.toLowerCase().includes(word))) {
     msg.delete();
   }
 });
