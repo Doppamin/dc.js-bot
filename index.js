230f08cbd1dd54f9a38a74039a7e7624a495129f
mtcnbzks <mtcnbzks@gmail.com>
2022-11-16 22:40:39 +0300

    u

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// const { token } = require("./config.json");
-require("dotenv").config();
+const { token } = require("./config.json"); 
 
 const fs = require("fs");
 const path = require("path");
@@ -10,54 +9,54 @@ const client = new Discord.Client();
 const myID = "313284967214088196";
 
 function isAttachImage(msgAttach) {
-    let url = msgAttach.url;
-    //True if this url is a png image.
-    return url.indexOf("png", url.length - "png".length /*or 3*/) !== -1;
+  let url = msgAttach.url;
+  //True if this url is a png image.
+  return url.indexOf("png", url.length - "png".length /*or 3*/) !== -1;
 }
 
 const greetingList = [
-    "sa",
-    "sea",
-    "selam",
-    "selamun aleyküm",
-    "selamunaleyküm",
+  "sa",
+  "sea",
+  "selam",
+  "selamun aleyküm",
+  "selamunaleyküm",
 ];
 
 const blacklistFile = fs.readFileSync(
-    path.join(__dirname, "blacklist.txt"),
-    "utf-8"
+  path.join(__dirname, "blacklist.txt"),
+  "utf-8"
 );
 const blacklist = blacklistFile.split("\n");
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", (msg) => {
-    // if msg contains any blacklist word
-    if (greetingList.includes(msg.content.toLowerCase())) {
-        msg.reply("Aleyküm selam.");
-    }
-    if (blacklist.some((word) => msg.content.toLowerCase().includes(word))) {
-        msg.delete();
-    }
-    if (msg.author.id === myID) {
-        msg.react("💯");
-    }
+  // if msg contains any blacklist word
+  if (greetingList.includes(msg.content.toLowerCase())) {
+    msg.reply("Aleyküm selam.");
+  }
+  if (blacklist.some((word) => msg.content.toLowerCase().includes(word))) {
+    msg.delete();
+  }
+  if (msg.author.id === myID) {
+    msg.react("💯");
+  }
 });
 
 client.on("message", (msg) => {
-    //if (msg.author.id == myID)
-    if (msg.channel.id === "1042119132293902466") {
-        if (msg.content.toUpperCase() === "HELLO THERE") {
-            msg.reply("General Kenobi!");
-        }
-        if (msg.attachments.size > 0) {
-            if (msg.attachments.every(isAttachImage)) {
-                msg.reply("Photo detected.");
-            }
-        }
+  //if (msg.author.id == myID)
+  if (msg.channel.id === "1042119132293902466") {
+    if (msg.content.toUpperCase() === "HELLO THERE") {
+      msg.reply("General Kenobi!");
     }
+    if (msg.attachments.size > 0) {
+      if (msg.attachments.every(isAttachImage)) {
+        msg.reply("Photo detected.");
+      }
+    }
+  }
 });
 
-client.login(process.env.TOKEN);
+client.login(token);
